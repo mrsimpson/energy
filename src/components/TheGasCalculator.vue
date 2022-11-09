a792e398190c0b4f68cc20ed3df81d685fdcb050
Oliver Jägle <github@beimir.net>
2022-11-10 00:08:38 +0100

    Calculate savings for 2023

--- src/components/TheGasCalculator.vue
+++ src/components/TheGasCalculator.vue
@@ -1,53 +1,63 @@
 <template>
-  <n-space horizontal>
-    <n-space vertical>
-      <n-card
-        class="calc-basis"
-        title="Meine Verbrauchsdaten"
-        style="max-width: 24rem"
+  <n-space vertical size="large">
+    <n-layout has-sider>
+      <n-layout-sider
+        collapse-mode="none"
+        :show-collapsed-content="false"
+        :collapsed-width="0"
+        width="30rem"
+        :show-trigger="false"
+        content-style="padding: 24px;"
+        bordered
       >
-        <n-space vertical>
-          <n-input-group>
-            <n-input-group-label>Energiebedarf</n-input-group-label>
-            <n-input-number
-              v-model:value="consumption"
-              :validator="validatePositive"
-              :min="1"
-              placeholder="Energiebedarf 2021"
-              :step="1000"
-            >
-              <template #suffix>kWh</template></n-input-number
-            >
-          </n-input-group>
-          <n-input-group>
-            <n-input-group-label>Gaspreis 2022</n-input-group-label>
-            <n-input-number
-              v-model:value="price2022"
-              :validator="validatePositive"
-              :min="0"
-              placeholder="Gaspreis 2021"
-              :step="0.01"
-              :format="euroToCent"
-              :parse="centToEuro"
-            >
-              <template #suffix>Cent / kWh</template></n-input-number
-            >
-          </n-input-group>
-          <n-el style="margin-top: 0.5rem; margin-bottom: 2rem">
-            <n-el>Einsparung 2023</n-el>
-            <n-slider
-              v-model:value="reduction2023"
-              :step="0.01"
-              :min="0"
-              :max="1"
-              :format-tooltip="(x) => `${(x * 100).toFixed(0)}%`"
-              :default-value="reduction2023"
-              :show-tooltip="true"
-              placement="bottom"
-            />
-          </n-el>
+        <n-space>
+          <n-card
+            class="calc-basis"
+            title="Meine Verbrauchsdaten"
+            style="max-width: 24rem"
+          >
+            <n-space vertical>
+              <n-input-group>
+                <n-input-group-label>Energiebedarf</n-input-group-label>
+                <n-input-number
+                  v-model:value="consumption"
+                  :validator="validatePositive"
+                  :min="1"
+                  placeholder="Energiebedarf 2021"
+                  :step="1000"
+                >
+                  <template #suffix>kWh</template></n-input-number
+                >
+              </n-input-group>
+              <n-input-group>
+                <n-input-group-label>Gaspreis 2022</n-input-group-label>
+                <n-input-number
+                  v-model:value="price2022"
+                  :validator="validatePositive"
+                  :min="0"
+                  placeholder="Gaspreis 2021"
+                  :step="0.01"
+                  :format="euroToCent"
+                  :parse="centToEuro"
+                >
+                  <template #suffix>Cent / kWh</template></n-input-number
+                >
+              </n-input-group>
+              <n-el style="margin-top: 0.5rem; margin-bottom: 2rem">
+                <n-el>Einsparung 2023</n-el>
+                <n-slider
+                  v-model:value="reduction2023"
+                  :step="100"
+                  :min="0"
+                  :max="consumption / 2"
+                  :format-tooltip="(x) => `${x.toFixed(0)}kWh`"
+                  :default-value="reduction2023"
+                  :show-tooltip="true"
+                  placement="bottom"
+                />
+              </n-el>
 
-          <!-- <n-input-group>
+              <!-- <n-input-group>
             <n-input-group-label>Abschlag 09/2022</n-input-group-label>
             <n-input-number
               v-model:value="paymentSeptember2022"
@@ -59,46 +69,55 @@
               <template #suffix>€</template></n-input-number
             >
           </n-input-group> -->
+            </n-space>
+          </n-card>
+          <n-card
+            class="comparison"
+            title="Zu Vergleichszwecken"
+            style="max-width: 24rem"
+          >
+            <n-space vertical>
+              <n-input-group>
+                <n-input-group-label>Gaspreis 2021</n-input-group-label>
+                <n-input-number
+                  v-model:value="price2021"
+                  :validator="validatePositive"
+                  :min="0"
+                  placeholder="Gaspreis 2021"
+                  :step="0.01"
+                  :format="euroToCent"
+                  :parse="centToEuro"
+                >
+                  <template #suffix>Cent / kWh</template></n-input-number
+                >
+              </n-input-group>
+              <n-input-group>
+                <n-input-group-label>Gaspreis 2023</n-input-group-label>
+                <n-input-number
+                  v-model:value="price2023"
+                  :validator="validatePositive"
+                  :min="0"
+                  placeholder="Gaspreis 2023"
+                  :step="0.01"
+                  :format="euroToCent"
+                  :parse="centToEuro"
+                >
+                  <template #suffix>Cent / kWh</template></n-input-number
+                >
+              </n-input-group>
+            </n-space>
+          </n-card>
         </n-space>
-      </n-card>
-      <n-card
-        class="comparison"
-        title="Zu Vergleichszwecken"
-        style="max-width: 24rem"
-      >
-        <n-space vertical>
-          <n-input-group>
-            <n-input-group-label>Gaspreis 2021</n-input-group-label>
-            <n-input-number
-              v-model:value="price2021"
-              :validator="validatePositive"
-              :min="0"
-              placeholder="Gaspreis 2021"
-              :step="0.01"
-              :format="euroToCent"
-              :parse="centToEuro"
-            >
-              <template #suffix>Cent / kWh</template></n-input-number
-            >
-          </n-input-group>
-          <n-input-group>
-            <n-input-group-label>Gaspreis 2023</n-input-group-label>
-            <n-input-number
-              v-model:value="price2023"
-              :validator="validatePositive"
-              :min="0"
-              placeholder="Gaspreis 2023"
-              :step="0.01"
-              :format="euroToCent"
-              :parse="centToEuro"
-            >
-              <template #suffix>Cent / kWh</template></n-input-number
-            >
-          </n-input-group>
-        </n-space>
-      </n-card>
-    </n-space>
-    <n-card title="Du sparst"> </n-card>
+      </n-layout-sider>
+      <n-layout-content>
+        <n-card class="savings" style="min-width: 40rem">
+          <p>Du sparst</p>
+          <p style="font-size: 3rem">{{`${calculation2023.savings.value.toFixed(2)}€`}}</p>
+          <p>Durch Deine Einsparung von {{reduction2023}}kWh reduzierst Du nicht nur Emissionen, ,sondern sparst auch richtig Geld!</p>
+          <p>Wie sich Deine Kosten entwickelt haben bzw. entwickeln können, siehst Du im folgenden Diagramm.</p>
+        </n-card>
+      </n-layout-content>
+    </n-layout>
   </n-space>
 </template>
 
@@ -110,25 +129,63 @@ import {
   NInputGroupLabel,
   NSpace,
   NSlider,
-  NEl
+  NEl,
+  NLayout,
+  NLayoutContent,
+  NLayoutSider,
 } from "naive-ui";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const consumption = ref(15000);
 const price2021 = ref(0.1);
 const price2022 = ref(0.18);
 const price2023 = ref(0.3);
-const reduction2023 = ref(0.1)
+const reduction2023 = ref(consumption.value * 0.1);
 const paymentSeptember2022 = ref((consumption.value * price2021.value) / 12);
+const subsidizedQuota = 0.8;
+const priceSubsidized2023 = 0.12;
 
 const validatePositive = (x: number) => x > 0;
 const euroToCent = (x: number | null): string =>
   `${((x || 0) * 100).toFixed(0)}`;
 const centToEuro = (x: string): number => Number(x) / 100;
+
+const reducedConsumption2023 = computed(
+  () => consumption.value - reduction2023.value
+);
+
+const calculation2023 = {
+  subsidized: computed(
+    (): number =>
+      Math.min(
+        subsidizedQuota * consumption.value,
+        reducedConsumption2023.value
+      ) * priceSubsidized2023
+  ),
+  market: computed(
+    (): number =>
+      Math.max(
+        reducedConsumption2023.value - calculation2023.subsidized.value,
+        0
+      ) * price2023.value
+  ),
+  savings: computed(
+    (): number =>
+      Math.min((1 - subsidizedQuota) * consumption.value, reduction2023.value) *
+        price2023.value +
+      Math.max(
+        reduction2023.value - (1 - subsidizedQuota) * consumption.value,
+        0
+      ) *
+        priceSubsidized2023
+  ),
+};
+
+// const savings2023 = computed( () => )
 </script>
 
 <style scoped>
-  .n-input-group-label {
-    min-width: 9rem;
-  }
+.n-input-group-label {
+  min-width: 9rem;
+}
 </style>
