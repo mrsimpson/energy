68a176e0da43e46f0b099cf7ee2ca7acc27a098e
Oliver Jägle <github@beimir.net>
2022-11-11 09:39:28 +0100

    Make chart reactive

--- src/components/ResultCharts.vue
+++ src/components/ResultCharts.vue
@@ -1,26 +1,27 @@
 <script setup lang="ts">
+import { toRef, toRefs } from "@vue/reactivity";
 import VueApexChart from "vue3-apexcharts";
 
-type Year = number
-type Amount = number
+type Year = number;
+type Amount = number;
 
 const props = defineProps<{
-  years: Year[]
-  bills: Amount[]
-  reductions: Amount[]
-  savings: Amount[]
-}>()
+  years: Year[];
+  bills: Amount[];
+  reductions: Amount[];
+  savings: Amount[];
+}>();
 
 const chartOptions = {
   chart: { id: "gas-costs", stacked: true },
   xaxis: { categories: props.years },
-}
-
+};
+const { bills, reductions, savings } = toRefs(props);
 const series = [
-  { name: "Eigenanteil", data: props.bills },
-  { name: "Staatliche Unterstützung", data: props.reductions },
-  { name: "Eigene Einsparung", data: props.savings },
-]
+  { name: "Eigenanteil", data: bills },
+  { name: "Staatliche Unterstützung", data: reductions },
+  { name: "Eigene Einsparung", data: savings },
+];
 </script>
 
 <template>
