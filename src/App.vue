bcdaad846315ce5ea511da495b5c01db2f4d5667
Oliver Jägle <github@beimir.net>
2022-11-16 11:26:59 +0100

    Enable theme editor

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
-<template>
-  <n-space vertical>
-    <n-config-provider :theme="theme">
-      <RouterView />
-    </n-config-provider>
-  </n-space>
-</template>
-
 <script setup lang="ts">
 import { ref } from "vue";
 import {
   NConfigProvider,
-  NSpace,
-  type GlobalTheme,
+  NGlobalStyle,
+  NThemeEditor,
+  type GlobalTheme, GlobalThemeOverrides,
 } from "naive-ui";
 
 // const osThemeRef = useOsTheme()
 //const theme = ref((osThemeRef.value === 'dark' ? darkTheme : null) as GlobalTheme | null)
 const theme = ref(null as GlobalTheme | null);
+
+const themeOverrides: GlobalThemeOverrides = {
+    common: {
+      primaryColor: '#f00000'
+    },
+  }
 </script>
+
+<template>
+  <n-theme-editor>
+    <n-config-provider :theme="theme" :theme-overrides="themeOverrides">
+      <RouterView />
+      <n-global-style />
+    </n-config-provider>
+  </n-theme-editor>
+</template>
