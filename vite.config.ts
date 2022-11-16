d9b1ccedfe7f69e3a8c20bc9dc677ff8ee9e1beb
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-11-16 15:10:23 +0100

    Allows deep links in gh pages (#41)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/energy/'
+  base: '/energy/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "404": resolve(__dirname, "404.html"),
+      },
+    },
+  }
 })
