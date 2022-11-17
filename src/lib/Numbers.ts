9d5db23b12f26614b9e24387e9c18af710048c4e
Oliver Jägle <github@beimir.net>
2022-11-17 09:59:14 +0100

    Allow empty reduction input

--- src/lib/Numbers.ts
+++ src/lib/Numbers.ts
@@ -6,7 +6,7 @@ export function euros(amount: number, fractionDigits = 2) {
   ) + " €";
 }
 
-export const validatePositive = (x: number) => x > 0;
+export const validatePositive = (x: number) => x >= 0;
 export const euroToCent = (x: number | null): string =>
   `${((x || 0) * 100).toFixed(0)}`;
 export const centToEuro = (x: string): number => Number(x) / 100;
