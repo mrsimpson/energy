f79a74896246ff0681489699f7e3b889d498a8fe
Oliver Jägle <github@beimir.net>
2022-11-18 19:02:13 +0100

    Add google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { createApp } from "vue";
+import VueGtagPlugin from "vue-gtag";
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
-app.use(router);
+app.
+    use(router)
+    .use(VueGtagPlugin, {
+        config: { id: "G-YPQNEW8RB9" }
+    })
 
 app.mount("#app");
