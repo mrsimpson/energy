1862def7510820ae0047f1b0cab91afb623dd2a0
Oliver Jägle <github@beimir.net>
2022-11-19 09:06:59 +0100

    Remove google analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import VueGtagPlugin from "vue-gtag";
 import App from "./App.vue";
 import router from "./router";
 
@@ -7,8 +6,5 @@ const app = createApp(App);
 
 app.
     use(router)
-    .use(VueGtagPlugin, {
-        config: { id: "G-DQW5PTK5L1", router, anonymize_ip: true}
-    })
 
 app.mount("#app");
