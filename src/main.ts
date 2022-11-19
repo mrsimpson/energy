41479eeaa07c53f397639343b35f4f0ccc3f8de6
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-11-19 21:46:45 +0100

    fix vue-matomo usage with typescript

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
-const VueMatomo = require('vue-matomo')
+import VueMatomo from "vue-matomo";
 
 const app = createApp(App);
 
-app
-    .use(router)
-    .use(VueMatomo, {
-        host: "https://matomo.dilab.co",
-        siteId: 2,
-        router,
-        disableCookies: true
-    })
+app.use(router).use(VueMatomo, {
+  host: "https://matomo.dilab.co",
+  siteId: 2,
+  router,
+  disableCookies: true,
+});
 
 app.mount("#app");
