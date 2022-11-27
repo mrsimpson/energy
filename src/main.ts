2c4080a6f918d57d087f2a8ae2cb505608a34c51
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2022-11-27 06:20:03 +0100

    Short campaign links (#96)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import App from "./App.vue";
 import router from "./router";
 import VueMatomo from "vue-matomo";
 
+const params = location.search
+  ? Object.fromEntries(
+      location.search
+        .replace(/^\?/, ``)
+        .split("&")
+        .map((p) => p.split("="))
+    )
+  : {};
+if (params.c) {
+  params.mtm_campaign = params.c;
+  if (["fedi", "twitter", "fb"].includes(params.c)) {
+    params.mtm_source = "social";
+  }
+  delete params.c;
+  const newSearch = Object.entries(params)
+    .map(([k, v]) => k + "=" + v)
+    .join("&");
+  location.href = location.pathname + "?" + newSearch;
+}
+
 const app = createApp(App);
 
 app.use(router).use(VueMatomo, {
